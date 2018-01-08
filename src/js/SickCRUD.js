e40be58c6e4ef994fc6b33a71ce27fe534d4d2f2
Emanuele Coppola <emanuele.coppola99@gmail.com>
2018-01-08 03:00:53 +0100

    Splitted SickCRUD initialize for convenience

--- src/js/SickCRUD.js
+++ src/js/SickCRUD.js
@@ -7,53 +7,6 @@ $(function() {
 
     });
 
-    // Hide show-password
-    $('input[type=password]').each(function (index) {
 
-        var $currentField = $(this),
-            $currentFieldBackgroundSize,
-            $currentFieldSpaceRightSize;
-
-        $currentField.hideShowPassword({
-            show: false,
-            innerToggle: 'focus',
-            toggle: {
-                className: 'text-toggle',
-            },
-            states: {
-                shown: {
-                    toggle: {
-                        content: $(this).attr('data-hide') || 'Hide',
-                    }
-                },
-                hidden: {
-                    toggle: {
-                        content: $(this).attr('data-show') || 'Show',
-                    }
-                }
-            }
-        });
-
-        // fix last-pass like services
-        setTimeout(function() {
-            $currentFieldBackgroundSize = $currentField.css('background-size').split(' ');
-            $currentFieldSpaceRightSize = 0;
-
-            for(var $currentFieldSide in $currentFieldBackgroundSize){
-                // if width or height is set
-                if($currentFieldBackgroundSize[$currentFieldSide]) {
-                    var $currentBackgroundSize = parseInt($currentFieldBackgroundSize[$currentFieldSide].slice(0, -2));
-                    // 35px as a threshold should be enough ?
-                    if($currentBackgroundSize < 35 && $currentBackgroundSize > $currentFieldSpaceRightSize) {
-                        $currentFieldSpaceRightSize = $currentBackgroundSize + (5 * (window.devicePixelRatio || 1));
-                    }
-                }
-            }
-
-
-            $currentField.parent('.hideShowPassword-wrapper').find('button[type=button]').css('right', $currentFieldSpaceRightSize + 'px');
-        }, 500);
-
-    });
 
 });
